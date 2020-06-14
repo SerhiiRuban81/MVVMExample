ca5381d5645b0e9dd787978a2ac805ae00fac7df
tetiana.ruban@hotmail.com <ruban_sa@i.ua>
2020-06-15 00:53:24 +0300

    Добавлен класс Person

---
+++ MVVMSample/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMSample.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Surname { get; set; }
+
+        public int Age { get; set; }
+    }
+}
